5079590a84ede75b3b872e76dabac11c8dbe0d14
vijay-varadarajan <vijayvaradarajan84@gmail.com>
2023-01-13 00:15:16 +0530

    latest

---
+++ week9/birthdays/Untitled-1.sql
@@ -0,0 +1,5 @@
+-- database: c:\Users\varad\Documents\flask_web_app\week9\birthdays\birthdays.db
+
+-- Shift+Enter or Ctrl+Shift+Q (query): executes a SELECT statement and displays the results in the table.
+-- Ctrl+Shift+E (executescript): executes the entire file without displaying the results.
+SELECT * FROM "birthdays";
