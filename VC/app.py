0c2db7392c288bee740ca5b184b59263abcc9000
vijay-varadarajan <vijayvaradarajan84@gmail.com>
2023-01-13 07:32:35 +0530

    latest

---
+++ VC/app.py
@@ -0,0 +1,23 @@
+# import cs50, flask 
+
+# Configure sessions 
+
+# Homepage
+
+# Register query route
+
+# Result (Vet clinics) display route
+
+# Login and signup routes
+
+# Logout - redirect to home page
+
+# User page route
+
+# User register pet route
+
+# User pet injections tracker route
+
+# Help / online counselling query route
+
+# Online help / counselling links display route
